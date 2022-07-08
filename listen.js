567b3860a149bba03389e4ca22ef6c8b208b4d5e
Mohamed Elrofai <m.elrofai@icloud.com>
2022-07-08 15:53:23 +0100

    set up heroku up

--- listen.js
+++ listen.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const PORT = 9090;
+const { PORT = 9090 } = process.env;
 app.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`nc_games app listening at http://localhost:${PORT}`);
